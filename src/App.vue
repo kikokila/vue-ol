6df037ed9bc2cf36e631a2a571efbbc9c8e05c98
chengjunyi <chengjunyi2019@163.com>
2023-07-23 15:05:32 +0800

    创建一个应用

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="count++">{{ count }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+var count = ref(1)
+
+</script>
+
+
+
+<style scoped></style>
