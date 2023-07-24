f2fd76f35383eaa2083aef8d0d22f61eda12acf2
chengjunyi <chengjunyi2019@163.com>
2023-07-24 18:11:50 +0800

    列表渲染

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <template>
-  <div v-if="flag">v-if渲染</div>
+  <form v-on:submit.prevent="addNewTodo">
+    <label for="new-todo">add a todo</label>
+    <input v-model="newTodoText" id="new-todo" placeholder="例如喂猫">
+    <button>增加</button>
+  </form>
   <br>
-  <div v-show="flag">v-show渲染</div>
-  <br>
-  <button @click="flag = !flag">点击改变状态</button>
+  <ul>
+    <TodoItem v-for="(todo, index) in todos" :key="todo.id" :title="todo.title" @remove="todos.splice(index, 1)"></TodoItem>
+  </ul>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, computed } from 'vue'
-var flag = ref(true)
+import TodoItem from './components/TodoItem.vue'
+const newTodoText = ref('')
+const todos = ref([
+  {
+    id: 1,
+    title: '做饭'
+  },
+  {
+    id: 2,
+    title: '打扫卫生'
+  },
+  {
+    id: 3,
+    title: '遛狗'
+  }
+])
+let nextTodoId = 4
+const addNewTodo = () => {
+  todos.value.push({
+    id: nextTodoId++,
+    title: newTodoText.value
+  })
+  newTodoText.value = ''
+}
+
+
 </script>
 
 
 
-<style scoped>
-.active {
-  font-size: 50px;
-}
-
-.helloWorld {
-  background-color: pink;
-}
-</style>
+<style scoped></style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>
+        {{ title }}
+        <button @click="$emit('remove')">删除</button>
+    </li>
+</template>
+
+<script setup lang="ts">
+defineProps(['title'])
+defineEmits(['remove'])
+</script>
+
+
+
+<style scoped>
+
+</style>
