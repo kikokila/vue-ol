31e4cce40583c5aa976a1a00afcdaabc25fbec29
chengjunyi <chengjunyi2019@163.com>
2023-07-25 10:42:25 +0800

    事件处理

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,56 @@
 <template>
-  <form v-on:submit.prevent="addNewTodo">
-    <label for="new-todo">add a todo</label>
-    <input v-model="newTodoText" id="new-todo" placeholder="例如喂猫">
-    <button>增加</button>
-  </form>
+  <div>
+    <input type="text" v-model.trim="uname">
+    过滤输入的首空格字符：{{ uname }}
+  </div>
   <br>
-  <ul>
-    <TodoItem v-for="(todo, index) in todos" :key="todo.id" :title="todo.title" @remove="todos.splice(index, 1)"></TodoItem>
-  </ul>
+  <div class="father" @click="fatherClick">
+    <a herf="http:www.baidu.com" @click.stop>百度(加了.stop)</a>
+    <br>
+    <a herf="http:www.baidu.com">百度2(未加.stop)</a>
+  </div>
+  <br>
+  <div @click.capture="one">
+    obj1(加了.capture)
+    <div @click.capture="two">
+      obj2(加了.capture)
+      <div @click="three">
+        obj3(未加.capture)
+        <div @click="four">
+          obj4(未加.capture)
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <div>
+    <button @click.left="getClick">点左键触发</button>
+    <button @click.middle="getClick">点中键触发</button>
+    <button @click.right="getClick">点右键触发</button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, computed } from 'vue'
-import TodoItem from './components/TodoItem.vue'
-const newTodoText = ref('')
-const todos = ref([
-  {
-    id: 1,
-    title: '做饭'
-  },
-  {
-    id: 2,
-    title: '打扫卫生'
-  },
-  {
-    id: 3,
-    title: '遛狗'
-  }
-])
-let nextTodoId = 4
-const addNewTodo = () => {
-  todos.value.push({
-    id: nextTodoId++,
-    title: newTodoText.value
-  })
-  newTodoText.value = ''
+const uname = ref('')
+const fatherClick = () => {
+  alert('.stop阻止冒泡')
 }
-
+const one = () => {
+  alert('one')
+}
+const two = () => {
+  alert('two')
+}
+const three = () => {
+  alert('three')
+}
+const four = () => {
+  alert('four')
+}
+const getClick = () => {
+  alert('触发成功')
+}
 
 </script>
 
